fe070fdec961943463e590eb32e1b3d1510ff871
Vitória de Oliveira da Silva <02vitoriaoliveira02@gmail.com>
2023-05-10 13:42:03 -0300

    views.py foi atualizada conforme a head.html

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Vitoria de Oliveira', 
     })
