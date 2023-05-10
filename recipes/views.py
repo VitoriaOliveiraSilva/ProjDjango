cfa771560b211294bd3f7ac189702905a6c749e7
Vitória de Oliveira da Silva <02vitoriaoliveira02@gmail.com>
2023-05-10 12:21:04 -0300

    Removidos contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
@@ -8,11 +7,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Vitoria de Oliveira', 
     })
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')  # /contato/
-
-
-def sobre(request):
-    return HttpResponse('Sobre nós!')  # /sobre/
